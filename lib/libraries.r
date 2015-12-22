ce04fbae7e5ebc6fb18e2744ae86f42e8644f689
PiaM123 <pia.modrijan94@gmail.com>
2015-12-22 18:44:10 +0100

    poprava

--- lib/libraries.r
+++ lib/libraries.r
@@ -9,5 +9,5 @@ require(gsubfn)
 library(XML)
 library(ggplot2)
 
-# Uvozimo funkcije za pobiranje in uvoz zemljevida.
+# Uvozimo funkcije za pobiranje in uvoz zemljevida..
 source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
