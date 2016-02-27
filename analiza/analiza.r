d7d459c4d0906ef52666c16193e43c0ac4435efd
PiaM123 <pia.modrijan94@gmail.com>
2016-02-27 17:15:56 +0100

    dodano

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -6,3 +6,15 @@
 grafŽV <- ggplot(ŽenskeVisoka, aes(x = Leto, y = Plača) ) + geom_point() + 
   geom_smooth(method = "lm", fullrange = TRUE) + xlim(2008, 2016)
 print(grafŽV)
+
+grafŽV1 <- lowess(ŽenskeVisoka$Leto, ŽenskeVisoka$Plača)
+grafŽV1 <- grafŽV + geom_line(data=as.data.frame(grafŽV1), aes(x=x, y=y), color="deeppink")
+print(grafŽV1)
+
+grafŽV2 <- loess(data = ŽenskeVisoka, Plača ~ Leto)
+grafŽV2 <- grafŽV + geom_smooth(method = "loess")
+print(grafŽV)
+
+grafŽV3 <- gam(data = ŽenskeVisoka, Plača ~ s(Leto))
+grafŽV3 <- fragŽV + geom_smooth(method = "gam", formula = y ~ s(x))
+print(grafŽV3)
